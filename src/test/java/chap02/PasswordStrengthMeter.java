73233ebcaab3790ecb09ed53095f0a3c69c4360e
Genie <devgeniegenie@gmail.com>
2023-09-06 11:15:27 +0900

    chap2 끝

--- src/test/java/chap02/PasswordStrengthMeter.java
+++ src/test/java/chap02/PasswordStrengthMeter.java
@@ -2,13 +2,42 @@ package chap02;
 
 public class PasswordStrengthMeter {
     public PasswordStrength meter(String s) {
-        if (s.length() < 8) {
-            return PasswordStrength.NORMAL;
-        }
-        boolean check = meetContainNumber(s);
-        if(!check) return PasswordStrength.NORMAL;
+        if(s == null || s.isEmpty()) return PasswordStrength.INVALID;
+
+        int meetCount = getMeetCount(s);
+
+        if(meetCount <= 1) return PasswordStrength.WEAK;
+        if(meetCount == 2) return PasswordStrength.NORMAL;
 
         return PasswordStrength.STRONG;
+    }
+
+    private int getMeetCount(String s) {
+        int meetCount = 0;
+        boolean lengthCheck = s.length() >= 8;
+        if(lengthCheck){
+            meetCount++;
+        }
+        boolean containNumber = meetContainNumber(s);
+        if(containNumber){
+            meetCount++;
+        }
+        boolean containUppercase = meetContainUppercase(s);
+        if(containUppercase){
+            meetCount++;
+        }
+        return meetCount;
+    }
+
+    private boolean meetContainUppercase(String s) {
+        boolean containUppercase = false;
+        for (char c : s.toCharArray()) {
+            if(Character.isUpperCase(c)) {
+                containUppercase = true;
+                break;
+            }
+        }
+        return containUppercase;
     }
 
     private boolean meetContainNumber(String s) {
--- src/test/java/chap02/PasswordStrengthMeterTest.java
+++ src/test/java/chap02/PasswordStrengthMeterTest.java
@@ -1,6 +1,5 @@
 package chap02;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -12,28 +11,64 @@ public class PasswordStrengthMeterTest {
      * 1. 길이가 8글자 이상
      * 2. 0부터 9 사이의 숫자를 포함
      * 3. 대문자 포함
-     *
+     * <p>
      * 모두만족 = 강함, 2개만족 = 보통, 1개만족 = 약함
      */
+
+    private PasswordStrengthMeter meter = new PasswordStrengthMeter();
+
+    private void assertStrength(String password, PasswordStrength expStr) {
+        PasswordStrength result = meter.meter(password);
+        assertEquals(expStr, result);
+    }
     @Test
     void meetsAllThenStrong() {
-        PasswordStrengthMeter meter = new PasswordStrengthMeter();
-        PasswordStrength result = meter.meter("absadf!@#2331ASD");
-        assertEquals(PasswordStrength.STRONG, result);
+        assertStrength("absadf!@#2331ASD", PasswordStrength.STRONG);
     }
 
     @Test
     void meetAllExceptLength() {
-        PasswordStrengthMeter meter = new PasswordStrengthMeter();
-        PasswordStrength result = meter.meter("s!@d4F");
-        assertEquals(PasswordStrength.NORMAL, result);
+        assertStrength("s!@d4F", PasswordStrength.NORMAL);
     }
 
     @Test
-    void meeAllExceptNumber() {
-        PasswordStrengthMeter meter = new PasswordStrengthMeter();
-        PasswordStrength result = meter.meter("s!@daaaabbbF");
-        assertEquals(PasswordStrength.NORMAL, result);
+    void meetAllExceptNumber() {
+        assertStrength("s!@daaaabbbF", PasswordStrength.NORMAL);
+    }
+
+    @Test
+    void nullInput() {
+        assertStrength(null, PasswordStrength.INVALID);
+    }
+
+    @Test
+    void emptyInput() {
+        assertStrength("", PasswordStrength.INVALID);
+    }
+
+    @Test
+    void meetAllExceptUppercase() {
+        assertStrength("sdfsd!344sdfsdf", PasswordStrength.NORMAL);
+    }
+
+    @Test
+    void meetOnlyLength() {
+        assertStrength("asdfsdfsfbsd", PasswordStrength.WEAK);
+    }
+
+    @Test
+    void meetOnlyNumber() {
+        assertStrength("1234", PasswordStrength.WEAK);
+    }
+
+    @Test
+    void meetOnlyUppercase() {
+        assertStrength("SDFS", PasswordStrength.WEAK);
+    }
+
+    @Test
+    void meetNothing() {
+        assertStrength("ab", PasswordStrength.WEAK);
     }
 
 }
